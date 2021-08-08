cf15ab437ccf47634297253a4807d06667fef3ba
mohit-cfc <santosh.mohit9@gmail.com>
2021-08-08 15:06:37 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const plButton = document.querySelector('#pl');
+plButton.addEventListener('click',function(){
+    location.href = "pl.html";
+});
+
+const laligaButton = document.querySelector('#la-liga');
+laligaButton.addEventListener('click',function(){
+    location.href = "la-liga.html";
+});
+
+const bundesligaButton = document.querySelector('#bundesliga');
+bundesligaButton.addEventListener('click',function(){
+    location.href = "bundesliga.html";
+});
+
+const ligue1Button = document.querySelector('#ligue-1');
+ligue1Button.addEventListener('click',function(){
+    location.href = "ligue-1.html";
+});
+
+const serieaButton = document.querySelector('#serie-a');
+serieaButton.addEventListener('click',function(){
+    location.href = "serie-a.html"
+})
+
+const uclButton = document.querySelector('#ucl');
+uclButton.addEventListener('click',function(){
+    location.href = "ucl.html"
+});
+
+const uelButton = document.querySelector('#uel');
+uelButton.addEventListener('click',function(){
+    location.href = "uel.html"
+})
